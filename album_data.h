a92e1cd64513e34e176125d1f29bd666f8e17e70
Shmuel Jacobs <tuf65651@astro.ocis.temple.edu>
2017-04-19 18:12:39 -0400

    header won't compile and I don't konw why

--- album_data.h
+++ album_data.h
@@ -1,11 +1,10 @@
 #ifndef ALBUM_DATA_H
 #define ALBUM_DATA_H
 
+const int MAX_FIELD = 40;
+
 typedef struct{
-    String title;
-    String artist;
-    String album;
-    int seconds;
+    char[] title, album, seconds;
 } song_t;
 
 void selection_sort_category(song_t[] listing, int category, int length)
@@ -15,6 +14,19 @@ void selection_sort_category(song_t[] listing, int category, int length)
 
     //inner counting loop will use this
     int index;
+	
+	for( sorted_until = 0; sorted_until < length; sorted_until++){
+		
+	}
 }
 
+int read_listing(FILE* input, song_t[])
+{
+	int song_number = 0;
+	char current_read;
+	int finished = 0;
+	while(!finished){
+		 current_read = fgets(input
+	}
+}
 #endif ALBUM_DATA_H
