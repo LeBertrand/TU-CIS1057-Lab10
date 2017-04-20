c903a8d9b39b1ec5d0b1eea6fc5537db3011aa75
Shmuel Jacobs <tuf65651@astro.ocis.temple.edu>
2017-04-20 17:05:26 -0400

    Created enum categories, fleshed out the selection sort and commented up

--- album_data.h
+++ album_data.h
@@ -3,14 +3,22 @@
 
 #ifndef ALBUM_DATA_H
 #define ALBUM_DATA_H
-
 #define MAX_ALBUM_ARTIST  70
 #define MAX_SONG_TITLE  25
 
+//song structure holds info of one song
 typedef struct{
     char title[MAX_SONG_TITLE], album[MAX_ALBUM_ARTIST],
 		seconds[MAX_ALBUM_ARTIST];
 } song_t;
+
+//array holds all song structures
+song_t all_songs[100];
+
+//explicit response coding makes passing category to sort function clearer
+typedef enum{
+	title, artist, time
+} cgry;
 
 void selection_sort_category(song_t listing[], int category, int length)
 {
@@ -20,8 +28,17 @@ void selection_sort_category(song_t listing[], int category, int length)
     //inner counting loop will use this
     int index;
 	
+	//visit every element in the list once
 	for( sorted_until = 0; sorted_until < length; sorted_until++){
-		
+		//check all items after the current one, looking for smallest
+		int loc_smallest = sorted_until;
+		for( index = sorted_until; index < length; index++){
+			//case: we found a new smallest
+			if(listing[index] < listing[loc_smallest]){
+				loc_smallest = index;
+			}
+		}//loop ends--we found smallest
+		//swaps go here
 	}
 }
 
